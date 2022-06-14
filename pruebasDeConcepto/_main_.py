f8ce3b2590b754bbcd0506742d0ec39d7fd0fbdd
GiovanniAlmazanB <104459611+GiovanniAlmazanB@users.noreply.github.com>
2022-06-14 10:25:46 -0500

    Add files via upload

---
+++ pruebasDeConcepto/_main_.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import pytz
+import os
+
+# flask library
+from flask_cors import CORS
+from flask import Flask
+
+# flask app
+app = Flask(__name__)
+CORS(app)
+
+from services.routes.v1.paths import v1
+# register blueprint
+
+app.register_blueprint(v1, url_prefix="/")
+
+port = int(os.environ.get('PORT', 8080))
+if __name__ == '__main__':
+    utc_now = datetime.utcnow()
+    utc_now = utc_now.astimezone(pytz.timezone('America/Mexico_City'))
+    print(utc_now)
+    app.run(threaded=True, host='0.0.0.0', port=port, debug=True)
---
+++ pruebasDeConcepto/utilities/bigquery.py
@@ -0,0 +1,80 @@
+#google library
+from urllib import response
+from google.cloud import bigquery
+
+#Standar library
+import requests
+import json
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C://Users/USER/Documents/keysAtlantia/ecomm-mx-jla054-modelorama-860e84ba1458.json"
+
+def priceIndex():
+    client=bigquery.Client()
+    response = []
+    query_job=client.query("""
+        SELECT * FROM `ecomm-mx-jla054-modelorama.views_bi.vw_product_prices` LIMIT 20
+            """)
+    results=query_job.result()
+    for i in results: 
+        response.append(i)
+    print(response)
+    return response
+
+def otherPromos(retailer):
+    client=bigquery.Client()
+    response = []
+    query_job = client.query(f"""
+        SELECT 
+            master_date_id,
+            retailer_id,
+            store_id,
+            master_product_id,
+            num_other_promos
+        FROM (
+            SELECT 
+                master_date_id, 
+                retailer_id, 
+                store_id, 
+                master_product_id, 
+                ARRAY_LENGTH(dwh.product_promotion_list) AS num_other_promos
+                -- other_promotion
+            FROM `views_bi.mv_ecommerce_products` AS dwh
+
+        ) AS tbl_other_promos
+        WHERE num_other_promos > 0 and retailer_id == {retailer}
+        
+            """)
+    results=query_job.result()
+    for i in results: 
+        response.append(i)
+    return response
+
+def bigQuery():
+    BigQuery_Client = bigquery.Client()
+    response = """
+
+    SELECT * FROM `ecomm-mx-jla054-modelorama.ecommerce_data.price_index` 
+            """
+    return response
+
+def productPromos():
+    client=bigquery.Client()
+    response = []
+    query_job=client.query("""
+        SELECT master_date_id,
+            retailer_id,
+            store_id,
+            master_product_id,
+            product_promo_price
+        FROM `views_bi.mv_ecommerce_products`
+        WHERE product_promo_price > 0
+        
+            """)
+    results=query_job.result()
+    for i in results: 
+        response.append(i)
+    print(response)
+    return response
+
+priceIndex()
